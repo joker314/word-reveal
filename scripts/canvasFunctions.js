06a7819f7415873d99b1d6edffff3bb29dfac08a
jokebookservice1 <joker314@users.noreply.github.com>
2018-06-13 21:20:18 +0100

    Update canvasFunctions.js

--- scripts/canvasFunctions.js
+++ scripts/canvasFunctions.js
@@ -1,26 +1,25 @@
 function display(ctx, canvas, text, bg, fg) {
-	console.log("Displaying result... ", arguments)
 	const w50 = canvas.width / 2
-  const h50 = canvas.height / 2
-
-  drawBackground(ctx, canvas, bg)
-  ctx.font = "50px Courier New"
-  ctx.fillStyle = fg
-  ctx.fillText(text, w50 - ctx.measureText(text).width / 2, h50)
+	const h50 = canvas.height / 2
+	
+	drawBackground(ctx, canvas, bg)
+	ctx.font = "50px Courier New"
+	ctx.fillStyle = fg
+	ctx.fillText(text, w50 - ctx.measureText(text).width / 2, h50)
 }
 
 function drawWord(ctx, canvas, points, word, color, cover) {
 	const w10 = 0.1 * canvas.width
-  const h50 = 0.5 * canvas.height
+	const h50 = 0.5 * canvas.height
+
 	ctx.font = "140px Courier New"
-  ctx.fillStyle = color
-  
-  ctx.fillText(word, w10 + 30, h50)
-  
-  const m = ctx.measureText(word)
-  
-  ctx.fillStyle = cover
-  ctx.fillRect(w10 + 30, h50 - 90 + 140 + (1 - 1.4 * points), m.width, 1.4 * points)
+	ctx.fillStyle = color
+	ctx.fillText(word, w10 + 30, h50)
+
+	const m = ctx.measureText(word)
+
+	ctx.fillStyle = cover
+	ctx.fillRect(w10 + 30, h50 - 90 + 140 + (1 - 1.4 * points), m.width, 1.4 * points)
 }
 
 function drawBackground(ctx, canvas, color) {
@@ -32,22 +31,22 @@ function drawCounter(ctx, canvas, score, penalty, good, bad) {
 	const percent = score / 100
 	const w10 = 0.1 * canvas.width
 	const h20 = 0.2 * canvas.height
-  
-  ctx.fillStyle = good
-  ctx.fillRect(w10, h20, 20, h20 * 3)
-  
-  ctx.fillStyle = bad
-  ctx.fillRect(w10, h20, 20, (1 - percent) * h20 * 3)
-  
-  ctx.font = "20px monospace"
-  ctx.fillText(Math.floor(score - penalty), w10, h20 - 30);
+	
+	ctx.fillStyle = good
+	ctx.fillRect(w10, h20, 20, h20 * 3)
+
+	ctx.fillStyle = bad
+	ctx.fillRect(w10, h20, 20, (1 - percent) * h20 * 3)
+
+ 	ctx.font = "20px monospace"
+ 	ctx.fillText(Math.floor(score - penalty), w10, h20 - 30);
 }
 
 function drawTotal(ctx, canvas, score, bg, fg) {
 	ctx.fillStyle = bg
 	ctx.fillRect(0, canvas.height - 10, canvas.width * (score / TARGET), 10)
   
-  ctx.fillStyle = fg
-  ctx.font = "20px monospace"
-  ctx.fillText(Math.floor(score), canvas.width * (score / TARGET) - 10, canvas.height - 15)
+	ctx.fillStyle = fg
+	ctx.font = "20px monospace"
+	ctx.fillText(Math.floor(score), canvas.width * (score / TARGET) - 10, canvas.height - 15)
 }
