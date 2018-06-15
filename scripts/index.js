f4446a1028c2e6b0e2a9588f25805882bdc7ac0d
jokebookservice1 <joker314@users.noreply.github.com>
2018-06-15 20:48:17 +0100

    Lucky this isn't imperial...
    Off by power of ten, again

--- scripts/index.js
+++ scripts/index.js
@@ -24,7 +24,7 @@ getWords().then(words => {
 		
 		const delta = now - gameState.startedAt
 		
-		gameState.points = 100 * (1 - delta / (ROUND_DURATION * 10e3))
+		gameState.points = 100 * (1 - delta / (ROUND_DURATION * 1000))
 		
 		if(Math.floor(gameState.points) <= 0) {
 			nextRound()
